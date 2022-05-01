63b46e0a2d8884015b0f0cf85df2de09c455128c
Andreas <andreas.goss1994@gmail.com>
2022-05-01 16:01:55 +0200

    Paint squares black when mouse hovers over them. Add clear button.

--- EtchASketch.js
+++ EtchASketch.js
@@ -4,9 +4,24 @@
  */
 const SQUARE_NUMBER = 256;
 let page = document.querySelector('.board');
+let clearButton = document.querySelector('button');
+
+clearButton.addEventListener('click', clearBoard);
 
 for(i=0; i<SQUARE_NUMBER; i++) {
     let square = document.createElement('div');
     square.classList.add('square');
+    square.addEventListener('mouseover', () => paintBlack(square), false);
     page.appendChild(square);
 }
+
+function paintBlack(square) {
+    square.setAttribute('style', 'background-color:black;');
+}
+
+function clearBoard() {
+    let allSquares= page.querySelectorAll('.square');
+    allSquares.forEach(s => s.setAttribute('style', 'background-color:white;'));
+}
+
+
